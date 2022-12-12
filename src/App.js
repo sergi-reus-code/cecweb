b2a83143b03abb84ac893c71923f2ee3e3ab8d3b
Sergi Reus <sergireus28@gmail.com>
2022-12-12 10:47:03 +0100

    sdgsdf

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p> Benvinguts a Estructures i Construccions Cerdanya S.L. </p>
+        <p> Sergi Reus </p>
         <a href="mailto:sergi@consestruccere.com">sergi@consestruccere.com</a>
       </header>
     </div>
