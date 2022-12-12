01bff296c07954f41dfe6ceb2a4a23a1d1273d36
sergi-reus-code <sergi@reusconstruccions.com>
2022-12-12 08:12:05 +0100

    ghkghkjgjk

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p> Benvinguts a Estructures i Construccions Cerdanya S.L. </p>
+        <a href="mailto:sergi@consestruccere.com">sergi@consestruccere.com</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
