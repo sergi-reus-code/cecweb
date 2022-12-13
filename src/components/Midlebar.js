81ebfdbf5425c2d92da41655ce201bbd6956b623
sergi-reus-code <sergi@reusconstruccions.com>
2022-12-13 08:52:47 +0100

    sdfgsdfg

---
+++ src/components/Midlebar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Midlebar() {
+  return (
+    <div>
+        <p> 
+            Benvinguts a Estructures i Construccions cerdanyadsfgsdf gsdf gñdfsglsdkfg dsf
+            lg hsdljfg dfgsjdfg sdñfgj sdf glsdfj gdljgfh sñldfl
+            ñgjh sdflñjg hsdlfjgh sdñfljgh sdñlfjgh ñlsdhf gdlsfjg
+            h lsjdfhg sdñfljgh ñlsdfhg ñljsdfh gñsdfgñlhjdswfg sd
+            fñlgjh uctures i Construccions Cerdanya S.L. </p>
+        
+        <hr className='hr'/>
+
+        <p> Sergi Reus </p>
+        
+        
+        
+        </div>
+  )
+}
+
+export default Midlebar
